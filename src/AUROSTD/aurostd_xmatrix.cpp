94294d87397059171135cdf2256e4924966f51be
Marco Esters <marco.esters@duke.edu>
2020-01-06 16:04:58 -0500

    Update src/AUROSTD/aurostd_xmatrix.cpp
    Co-Authored-By: David Hicks <d.hicks@duke.edu>

--- src/AUROSTD/aurostd_xmatrix.cpp
+++ src/AUROSTD/aurostd_xmatrix.cpp
@@ -733,7 +733,7 @@ namespace aurostd {  // namespace aurostd
     }
 
     xmatrix<xcomplex<utype> > c(a.rows, b.cols);
-    int i, j, k, ii, jj, kk;
+    int i=0, j=0, k=0, ii=0, jj=0, kk=0;
     utype *ai, aik;
     xcomplex<utype> *bk, *ci;
     for (i = c.lrows, ii = a.lrows; i <= c.urows; i++, ii++) {
