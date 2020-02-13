7933def667c23b0c5e6f150585879f3e1f6f8ddb
coreyoses <coreyoses@users.noreply.github.com>
2020-02-13 16:13:36 -0500

    Update src/aflow_xproto.cpp
    Co-Authored-By: Marco Esters <marco.esters@duke.edu>

--- src/aflow_xproto.cpp
+++ src/aflow_xproto.cpp
@@ -2016,7 +2016,7 @@ namespace aflowlib {
         if(nn_dist<_XPROTO_TOO_CLOSE_ERROR_){ //CO+DX200213
           if(flip_option==FALSE && SpaceGroupOptionRequired(str.spacegroupnumber)==TRUE) {  //CO+DX200213
             // *voss << "AFLOW WARNING (aflow_xproto.cpp): label=" << label << " WRONG NNdist too close =" << nn_dist << "   *************"  << endl;
-            *voss << "AFLOW WARNING (aflow_xproto.cpp): Too close NNdist(" << nn_dist << "), Spacegroup=" << str.spacegroupnumber << " option=" << str.spacegroupnumberoption << " not enough, try flip_option " << endl;
+            *voss << "AFLOW WARNING (aflow_xproto.cpp): Too close NNdist(" << nn_dist << "), Spacegroup=" << str.spacegroupnumber << " option=" << str.spacegroupnumberoption << " not enough, trying flip_option " << endl;
             xstructure str_flipped;
             str_flipped=aflowlib::PrototypeLibraries(*voss,label,parameters,vatomX,vvolumeX,volume_in,mode,TRUE);
             str_flipped.title+=" (sg.opt flipped)";
