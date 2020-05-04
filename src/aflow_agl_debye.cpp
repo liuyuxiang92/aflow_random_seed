98f0747ff8e7a17f54c011f7dccc776d5c2a59e1
coreyoses <coreyoses@users.noreply.github.com>
2020-05-04 19:59:16 -0400

    Update src/aflow_agl_debye.cpp
    Co-authored-by: David Hicks <david.hicks@duke.edu>

--- src/aflow_agl_debye.cpp
+++ src/aflow_agl_debye.cpp
@@ -524,7 +524,7 @@ namespace AGL_functions {
   uint Get_ThermalProperties_AGL_postprocess(string directory, const uint& ntemperature, const double& stemperature, const uint& npressure, const double& spressure, vector<double>& agl_temperature, vector<double>& agl_gibbs_energy_atom, vector<double>& agl_vibrational_energy_atom) {  
     // Class to contain AGL input and output data
     _AGL_data AGL_data;
-    uint aglerror;
+    uint aglerror = 0;
     ostringstream aus;
     _xvasp xvasp;
     string AflowIn;
