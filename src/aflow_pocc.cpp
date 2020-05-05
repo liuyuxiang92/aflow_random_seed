374c84c659aec5590d8da85fba9ece2ba216bf1a
Marco Esters <mme21@duke.edu>
2020-05-04 20:01:37 -0400

    Small typo

--- src/aflow_pocc.cpp
+++ src/aflow_pocc.cpp
@@ -6924,6 +6924,6 @@ ostream& operator<<(ostream& oss, const pocc::POccStructuresFile& psf) {
 // ***************************************************************************
 // *                                                                         *
 // *              AFlow STEFANO CURTAROLO  Duke University 2003-2020         *
-// *              AFlow COREY OSES  Duke University 2013-2012                *
+// *              AFlow COREY OSES  Duke University 2013-2020                *
 // *                                                                         *
 // ***************************************************************************
