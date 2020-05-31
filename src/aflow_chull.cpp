d03aedd585e3f9d0edfe5a9c5e40284331b74b24
Corey Oses <corey.oses@gmail.com>
2020-05-31 15:56:10 -0400

    removing whitespaces from chull input injected from web

--- src/aflow_chull.cpp
+++ src/aflow_chull.cpp
@@ -239,6 +239,7 @@ namespace chull {
       if(vpflow.flag("CHULL::SCREEN_ONLY")&&vpflow.flag("CHULL::JSON_DOC")){oss << "{}";} //so JSON-reader doesn't bomb
       return FALSE;
     }
+    inputs=aurostd::RemoveWhiteSpaces(inputs);  //CO20200531 - sometimes web injects spaces
     vector<string> tokens_comma;
     uint nary;
     string original_input;
