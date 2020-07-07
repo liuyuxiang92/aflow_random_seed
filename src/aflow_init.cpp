5649420ac286e72446bd2c4d63db9c6f1918b0dd
Marco Esters <marco.esters@duke.edu>
2020-07-07 12:15:20 -0400

    Fixed AFLOW_Projects_Directories to not break when a Lib directory is missing

--- src/aflow_init.cpp
+++ src/aflow_init.cpp
@@ -1423,18 +1423,20 @@ namespace init {
     bool LDEBUG=FALSE;
     if(LDEBUG) {;} //CO20190906 - keep LDEBUG busy
     string out="";
-    if(lib=="AUID" || lib=="auid") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_AUID);
-    if(lib=="ICSD" || lib=="icsd") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_ICSD);
-    if(lib=="LIB0" || lib=="lib0" ||  lib=="0") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB0);
-    if(lib=="LIB1" || lib=="lib1" ||  lib=="1") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB1);
-    if(lib=="LIB2" || lib=="lib2" ||  lib=="2") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB2);
-    if(lib=="LIB3" || lib=="lib3" ||  lib=="3") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB3);
-    if(lib=="LIB4" || lib=="lib4" ||  lib=="4") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB4);
-    if(lib=="LIB5" || lib=="lib5" ||  lib=="5") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB5);
-    if(lib=="LIB6" || lib=="lib6" ||  lib=="6") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB6);
-    if(lib=="LIB7" || lib=="lib7" ||  lib=="7") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB7);
-    if(lib=="LIB8" || lib=="lib8" ||  lib=="8") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB8);
-    if(lib=="LIB9" || lib=="lib9" ||  lib=="9") out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB9);    
+    //ME20200707 - The LIBRARY_NOTHING check is important or this function
+    //breaks when the LIB directory does not exist
+    if((XHOST_LIBRARY_AUID != LIBRARY_NOTHING) && (lib=="AUID" || lib=="auid")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_AUID);
+    if((XHOST_LIBRARY_ICSD != LIBRARY_NOTHING) && (lib=="ICSD" || lib=="icsd")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_ICSD);
+    if((XHOST_LIBRARY_LIB0 != LIBRARY_NOTHING) && (lib=="LIB0" || lib=="lib0" ||  lib=="0")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB0);
+    if((XHOST_LIBRARY_LIB1 != LIBRARY_NOTHING) && (lib=="LIB1" || lib=="lib1" ||  lib=="1")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB1);
+    if((XHOST_LIBRARY_LIB2 != LIBRARY_NOTHING) && (lib=="LIB2" || lib=="lib2" ||  lib=="2")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB2);
+    if((XHOST_LIBRARY_LIB3 != LIBRARY_NOTHING) && (lib=="LIB3" || lib=="lib3" ||  lib=="3")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB3);
+    if((XHOST_LIBRARY_LIB4 != LIBRARY_NOTHING) && (lib=="LIB4" || lib=="lib4" ||  lib=="4")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB4);
+    if((XHOST_LIBRARY_LIB5 != LIBRARY_NOTHING) && (lib=="LIB5" || lib=="lib5" ||  lib=="5")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB5);
+    if((XHOST_LIBRARY_LIB6 != LIBRARY_NOTHING) && (lib=="LIB6" || lib=="lib6" ||  lib=="6")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB6);
+    if((XHOST_LIBRARY_LIB7 != LIBRARY_NOTHING) && (lib=="LIB7" || lib=="lib7" ||  lib=="7")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB7);
+    if((XHOST_LIBRARY_LIB8 != LIBRARY_NOTHING) && (lib=="LIB8" || lib=="lib8" ||  lib=="8")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB8);
+    if((XHOST_LIBRARY_LIB9 != LIBRARY_NOTHING) && (lib=="LIB9" || lib=="lib9" ||  lib=="9")) out=vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_LIB9);
     return out;
 
     //subst "vAFLOW_PROJECTS_DIRECTORIES.at(XHOST_LIBRARY_AUID)" "init::AFLOW_Projects_Directories(\"AUID\")" *cpp
