4cc062875457f8baa0f1fb29ad7f787424d8e216
davidjhicks <david.hicks@duke.edu>
2020-09-01 12:32:18 -0400

    - Added missing files (for TravisCI) - Cleaning code

--- src/aflow_anrl.h
+++ src/aflow_anrl.h
@@ -59,57 +59,7 @@ namespace anrl{
   // checking functions
   bool structureAndLabelConsistent(const xstructure& _xstr, const string& label_input, string& label_and_params_calculated);
 }
-//SYMBOLIC
-/*
-#ifdef COMPILE_SYMBOLIC
-//SYMBOLIC
 
-// ---------------------------------------------------------------------------
-// mirrors wyckoffsite_ITC, requires SymbolicC++ 
-// instead of adding Symbolic equations to wyckoffsite_ITC (don't want to put 
-// large dependency in aflow.h)
-struct SymbolicWyckoffSite {
-  xvector<double> coord;
-  uint index;
-  string type;
-  string wyckoffSymbol;
-  string letter;
-  string site_symmetry;
-  uint multiplicity;
-  double site_occupation;
-  vector<symbolic::Symbolic> equations;
-  uint parameter_index;
-};
+// Symbolic functions are defined in aflow_symbolic.cpp
 
-SymbolicWyckoffSite initializeSymbolicWyckoffSite(const wyckoffsite_ITC& Wyckoff);
-void substituteVariableWithParameterDesignation(vector<SymbolicWyckoffSite>& Wyckoff_sites_symbolic);
-void substituteVariableWithParameterDesignation(SymbolicWyckoffSite& Wyckoff_symbolic);
-
-// ---------------------------------------------------------------------------
-// "pure" symbolic functions 
-namespace symbolic {
-  Symbolic string2symbolic(const string& str);
-  bool isEqual(const Symbolic& a, const Symbolic& b);
-  bool isEqualVector(const Symbolic& a_vec, const Symbolic& b_vec);
-  vector<vector<string> > matrix2VectorVectorString(const Symbolic& lattice);
-  Symbolic BringInCell(const Symbolic& vec_in, double tolerance=_ZERO_TOL_, double upper_bound=1.0, double lower_bound=0.0);
-}
-
-// ---------------------------------------------------------------------------
-// ANRL symbolic functions 
-namespace anrl {
-  symbolic::Symbolic SymbolicANRLPrimitiveLattices(const string& lattice_and_centering, const char& space_group_letter);
-  vector<symbolic::Symbolic> equations2SymbolicEquations(const vector<vector<string> >& equations);
-  symbolic::Symbolic cartesian2lattice(const symbolic::Symbolic& lattice, const symbolic::Symbolic& cartesian_coordinate);
-  symbolic::Symbolic getXYZ2LatticeTransformation(const string& lattice_and_centering);
-  vector<symbolic::Symbolic> getEquationsForCenteredLattices(const string& lattice_and_centering,
-      const symbolic::Symbolic& lattice,
-      const vector<symbolic::Symbolic>& conventional_equations);
-  vector<symbolic::Symbolic> convertEquations2FractionalEquations(const string& lattice_and_centering,
-      const symbolic::Symbolic& lattice,
-      const vector<symbolic::Symbolic> conventional_equations);
-  void addSymbolicEquation2Atoms(const vector<symbolic::Symbolic>& equations, deque<_atom>& atoms, bool isfpos=true);
-}
-#endif 
-*/
 #endif 
