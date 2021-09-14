32d07db0eccab1b60f10616f1a88a6704342d7a3
Andriy Smolyanyuk <andriy.smolyanyuk@duke.edu>
2021-09-14 13:13:50 -0400

    Adding license header

--- src/aflow_pocc_qha.cpp
+++ src/aflow_pocc_qha.cpp
@@ -6,6 +6,22 @@
 //****************************************************************************
 // Written by Andriy Smolyanyuk, 2021.
 //
+//    This file is part of Aflow.
+//
+//    Aflow is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Aflow is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Aflow.  If not, see <https://www.gnu.org/licenses/>.
+
+
 // This file provides a framework to calculate thermal properties for
 // disordered materials modeled using the POCC + QHA methodology.
 
