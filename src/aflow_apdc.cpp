323039b5e051a2da8da35f1a0d4b42f0d5645c79
Simon Divilov <simon.divilov@duke.edu>
2022-02-18 16:44:51 -0500

    Added APDC files

---
+++ src/aflow_apdc.cpp
@@ -0,0 +1,67 @@
+//****************************************************************************
+// *                                                                         *
+// *           Aflow STEFANO CURTAROLO - Duke University 2003-2022           *
+// *                  Simon Divilov - Duke University 2022                   *
+// *                                                                         *
+//****************************************************************************
+// Written by Simon Divilov, 2022
+// simon.divilov@duke.edu
+// 
+#ifndef _AFLOW_APDC_CPP_
+#define _AFLOW_APDC_CPP_
+#include "aflow.h"
+#include "aflow_apdc.h"
+
+// ###############################################################################
+//            AFLOW Automatic Phase Diagram Constructor (APDC) (2022-)
+// ###############################################################################
+
+// **************************************************************************
+// Class _apdc data
+// **************************************************************************
+// Constructor
+_apdc_data::_apdc_data() {
+  // Input data
+  rundirpath = "";
+  plattice = "";
+  elements.clear();
+
+  // Calculated data
+}
+
+// Destructor
+_apdc_data::~_apdc_data() {
+  free();
+}
+void _apdc_data::free() {
+}
+
+// Copy constructor
+const _apdc_data& _apdc_data::operator=(const _apdc_data &b) {
+  if (this != &b) {
+    rundirpath = b.rundirpath;
+    plattice = b.plattice;
+    elements = b.elements;
+  }
+  return *this;
+}
+
+// ***************************************************************************
+// apdc::GetPhaseDiagram
+// ***************************************************************************
+namespace apdc {
+  void GetPhaseDiagram(_apdc_data& apdc_data) {
+    cerr << "TESTING" << endl;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ src/aflow_apdc.h
@@ -0,0 +1,37 @@
+//****************************************************************************
+// *                                                                         *
+// *           Aflow STEFANO CURTAROLO - Duke University 2003-2022           *
+// *                  Simon Divilov - Duke University 2022                   *
+// *                                                                         *
+//****************************************************************************
+// Written by Simon Divilov, 2022
+// simon.divilov@duke.edu
+// 
+#ifndef _AFLOW_APDC_H_
+#define _AFLOW_APDC_H_
+
+// Class _apdc data
+class _apdc_data {
+  public:
+    _apdc_data();
+    ~_apdc_data();
+    const _apdc_data& operator=(const _apdc_data &b);
+    
+    // Input data
+    string rundirpath;
+    string plattice;
+    vector<string> elements;
+
+    // Calculated data
+  
+
+  private:
+    void free();
+};
+
+// Namespace for functions used by APDC
+namespace apdc {
+  void GetPhaseDiagram(_apdc_data& apdc_data);
+}
+
+#endif
