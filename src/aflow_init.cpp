eff1ec34f0e8fbff3f7db1e1c027340f27add33d
Hagen Eckert <hagen.eckert@duke.edu>
2022-07-01 16:57:46 -0400

    fix segfault when no user-name is set (getenv nullptr)

--- src/aflow_init.cpp
+++ src/aflow_init.cpp
@@ -67,13 +67,17 @@ namespace init {
     // AFLOWRC LOAD DEFAULTS FROM AFLOWRC.
     // XHOST.aflowrc_filename=AFLOWRC_FILENAME_LOCAL;
     // XHOST.vflag_control.flag("AFLOWRC::OVERWRITE",aurostd::args2flag(XHOST.argv,cmds,"--aflowrc=overwrite|--aflowrc_overwrite"));
-    XHOST.home=getenv("HOME");  //AS SOON AS POSSIBLE
-    XHOST.user=getenv("USER");  //AS SOON AS POSSIBLE
+    // getenv returns a char pointer, that can be null if the environment variable is not set
+    // assigning a nullptr to a string is undefined behavior
+    // always use aurostd::getenv2string that contains the necessary check HE20220701 //SD20220701
+    XHOST.home = aurostd::getenv2string("HOME"); //AS SOON AS POSSIBLE
+    XHOST.user = aurostd::getenv2string("USER");  //AS SOON AS POSSIBLE
     if(!aflowrc::is_available(oss,INIT_VERBOSE || XHOST.DEBUG)) aflowrc::write_default(oss,INIT_VERBOSE || XHOST.DEBUG);
     aflowrc::read(oss,INIT_VERBOSE || XHOST.DEBUG);
     XHOST.vflag_control.flag("AFLOWRC::READ",aurostd::args2flag(XHOST.argv,cmds,"--aflowrc=read|--aflowrc_read"));
     if(XHOST.vflag_control.flag("AFLOWRC::READ")) {aflowrc::print_aflowrc(oss,TRUE);return false;}
     // SD20220223 - read AFLOWRC to determine the temporary directory, which is needed for execute2string
+
     // SD20220223 - check to make sure the temporary directory is writable
     vector<string> tokens;
     string tmpfs_str=DEFAULT_TMPFS_DIRECTORIES;
